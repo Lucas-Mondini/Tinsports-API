eeee3f230731e8a6d660dfa328194a2ae293dccf
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2021-03-20 15:02:29 -0300

    edit

--- src/routes.ts
+++ src/routes.ts
@@ -11,5 +11,6 @@ routes.get('/', (req, res) => {
 routes.get('/user',     registerController.index);
 routes.get('/user/:name', registerController.getByName);
 routes.post('/user',    registerController.save);
+routes.delete('/user/:_id', registerController.destroy);
 
 export default routes;
