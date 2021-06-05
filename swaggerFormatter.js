0c45ca4752b9f54527971d638130dfe54d6595c9
JeanCDev <jeangames15@gmail.com>
2021-06-04 21:10:59 -0300

    Salvando e json e token de exemplo

--- swaggerFormatter.js
+++ swaggerFormatter.js
@@ -7,9 +7,11 @@ for (path in swaggerData.paths) {
     if(path == "/register/user" && method == "post" || path == "/login") continue;
     const params = swaggerData.paths[path][method]['parameters'].push({
       name: "auth_token",
-      in: "header"
+      in: "header",
+      required: true,
+      example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGE5MjgxNDRkMWFmOTA1ZThkNTk0MjkiLCJpYXQiOjE2MjI4NDgyOTh9.GYtB6ed3XfrZDDXB8oGd7jQUxlUfgIIpA-uo1eIb9LQ"
     });
   }
 }
 
-fs.writeFile("swagger-output.txt", JSON.stringify(swaggerData), () => console.log('Data formatted!'));
+fs.writeFile("swagger-output.json", JSON.stringify(swaggerData), () => console.log('Data formatted!'));
