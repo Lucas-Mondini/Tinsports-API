1f18ce8a807e2d6e1e322cd79b77a775b86b03eb
JeanCDev <jeangames15@gmail.com>
2021-08-07 11:37:31 -0300

    Cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express from 'express';
-import mongo from './DB/mongo';
+import cors from 'cors';
 import 'dotenv/config';
 
+import mongo from './DB/mongo';
 import { friendRoutes } from './routes/FriendRoutes';
 import { userRoutes } from './routes/UserRoutes';
 import { gameRoutes } from './routes/GameRoutes';
 import { gameListRoutes } from './routes/GameListRoutes';
 
+const corsOptions = {
+  origin: "*",
+  optionSuccessStatus: 200
+}
+
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(userRoutes);
 app.use(gameRoutes);
@@ -17,6 +24,6 @@ app.use(gameListRoutes);
 
 mongo.connect();
 
-app.listen(process.env.PORT, ()=> {
+app.listen(process.env.PORT, () => {
   console.log('Server Started');
 });
