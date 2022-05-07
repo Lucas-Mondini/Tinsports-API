d8c583a439734c2f6f9078f1466419fd7e8bb54a
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2022-05-07 14:38:04 -0300

    testing the difference between moments and batch updating

--- src/utils/Scheduler.ts
+++ src/utils/Scheduler.ts
@@ -24,6 +24,7 @@ export default class Scheduler
     const games = await Game.find({recurrence: true, finished: false});
 
     for (const game of games) {
+      // @ts-ignore
       const now = moment().tz("America/Sao_Paulo").format('YYYY-MM-DD[T]HH:mm'),
             gameDate = moment(game.date).tz("America/Sao_Paulo").format('YYYY-MM-DD[T]HH:mm'),
             nowNumber = Number(new Date(now)),
@@ -40,10 +41,17 @@ export default class Scheduler
   async premiumUpdate() {
     let userController = new UserController();
     let users = await userController.getAllUsers();
+    let updatedUsers = new Array();
 
     for (let user of users) {
-        //comparar a data
+        // @ts-ignore
+      if (moment(user.premiumTill).diff(moment().tz("America/Sao_Paulo").format('YYYY-MM-DD[T]HH:mm')) >= 0) {
+          user.premiumTill = null;
+          user.premium = false;
+          updatedUsers.push(user);
+        }
     }
+    await users.updateMany(updatedUsers);
 
   }
 
@@ -52,7 +60,7 @@ export default class Scheduler
     let cronJob: CronJob  = new cron.CronJob("0 1 * * *", async () => {
       logger.info("Executed schedule update");
       await ScheduleController.getInstance(true);
-      await this.retrieve();
+      this.retrieve();
     }, null, false, 'America/Sao_Paulo');
 
     cronJob.start();
